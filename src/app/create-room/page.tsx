3bc1b938f268d6ce37284de5adcce4ebc32a5b8c
Prajwal-PL <prajwalpl096@gmail.com>
2024-09-05 20:20:02 +0530

    Adding authentication pages, enhancing navbar and starting to build browse page

--- src/app/create-room/page.tsx
+++ src/app/create-room/page.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import NewRoomForm from "./new-room-form";
+import { auth } from "../../../auth";
+import { redirect } from "next/navigation";
 
 type Props = {};
 
-const NewRoomPage = (props: Props) => {
+const NewRoomPage = async (props: Props) => {
+  const session = await auth();
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <div className="container px-12 md:px-0 mx-auto flex justify-center flex-col max-w-screen-lg mb-8">
       <div className="space-y-2 mb-8 md:mb-14 mt-4">
--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
 import { ModeToggle } from "../ui/mode-toggle";
+import { auth } from "../../../auth";
+import Image from "next/image";
 
 type Props = {};
 
-const Navbar = (props: Props) => {
+const Navbar = async (props: Props) => {
+  const session = await auth();
   return (
     <div className="flex items-center justify-between mx-auto backdrop-blur-md backdrop-brightness-75 dark:bg-black/20 top-0 sticky z-50 max-w-screen-sm sm:max-w-screen-2xl p-3">
       <div>
@@ -13,13 +16,24 @@ const Navbar = (props: Props) => {
           Edge
         </Link>
       </div>
-      <div className="flex items-center gap-3">
+      <div className="flex items-center">
+        {!session?.user ? (
+          <Link href={"/login"}>
+            <button className="px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200">
+              Sign In
+            </button>
+          </Link>
+        ) : (
+          <div className="flex items-center">
+            <Button
+              className="text-xs sm:text-sm font-semibold"
+              variant={"link"}
+            >
+              {session?.user?.name}
+            </Button>
+          </div>
+        )}
         <ModeToggle />
-        <Link href={"#"}>
-          <button className="px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200">
-            Sign In
-          </button>
-        </Link>
       </div>
     </div>
   );
